e7b6cbd34f002b260bfd4d8e43ebd77cf6f7b8a2
Caique <caique_giovannini@hotmail.com>
2020-05-03 12:59:52 -0300

    added deliverers page

--- src/routes.js
+++ src/routes.js
@@ -2,23 +2,24 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
+
     return res.render('home/index')
 })
-routes.get('/search', (req, res) => {
-    let stores = []
+routes.get('/deliverers', (req, res) => {
+    let deliverers = []
 
     for (i=1; i<=10; i++) {
-        stores.push({
+        deliverers.push({
             photo: 'https://via.placeholder.com/500x500',
-            name: `Loja ${i}`,
-            description: 'Vendemos máscaras e produtos de limpeza'
+            name: `Entregador ${i}`,
+            description: 'Avaliação ou alguns dados'
         })
     }
 
-    return res.render('home/search', { stores })
+    return res.render('home/deliverers', { deliverers })
 })
 routes.get('/users/register', (req, res) => {
-
+    
     return res.render('user/register')
 })
 routes.get('/users/login', (req, res) => {
@@ -26,5 +27,18 @@ routes.get('/users/login', (req, res) => {
     return res.render('session/login')
 })
 
+routes.get('/sellers', (req, res) => {
+    let stores = []
+
+    for (i=1; i<=8; i++) {
+        stores.push({
+            photo: 'https://via.placeholder.com/500x500',
+            name: `Loja ${i}`,
+            description: 'Vendemos máscaras e produtos de limpeza'
+        })
+    }
+
+    return res.render('home/sellers', { stores })
+})
 
 module.exports = routes
