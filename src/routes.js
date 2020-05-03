2aede601ea75b849cd3576a91f487f2db07d903d
Caique <caique_giovannini@hotmail.com>
2020-05-03 09:07:29 -0300

    added express server and template engine nunjucks

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const routes = express.Router()
+
+routes.get('/', (req, res) => {
+    return res.render('home/index')
+})
+routes.get('/search', (req, res) => {
+    let stores = []
+
+    for (i=1; i<=10; i++) {
+        stores.push({
+            photo: 'https://via.placeholder.com/500x500',
+            name: `Loja ${i}`,
+            description: 'Vendemos máscaras e produtos de limpeza'
+        })
+    }
+
+    return res.render('home/search', { stores })
+})
+
+
+module.exports = routes
